698ebc08567a72170fec9c25824be88fa8b6ad35
Jeremy Springer <Jeremyspringer50@gmail.com>
2016-12-01 15:01:34 -0500

    help us all

--- app/Model/User.js
+++ app/Model/User.js
@@ -7,14 +7,14 @@ class User extends Lucid {
 	
 
 	static get hidden(){
-		return['password']
+		return ['password']
 	}
 
   apiTokens () {
 
     return this.hasMany('App/Model/Token')
   }
-  
+
  location(){
   return this.hasOne('App/Model/Location')
 
