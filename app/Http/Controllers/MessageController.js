6cbb21acf59d27933978bf45a91c01cf5974ead9
Jeremy Springer <Jeremyspringer50@gmail.com>
2016-12-06 19:46:39 -0500

    test

--- app/Http/Controllers/MessageController.js
+++ app/Http/Controllers/MessageController.js
@@ -28,11 +28,8 @@ class MessageController {
     * show (request, response){
     	let friend_id = request.param('friend_id')
     	let user = request.authUser
-    	console.log(friend_id, user.id)
-    	let message = yield Message.query()
-    		.where("sender_id", friend_id)
-    	// let message = yield Message.query().whereRaw("sender_id = ? AND recipient_id = ? OR sender_id = ? AND recipient_id = ?",
-    	// 	[user.id, friend_id, friend_id, user.id]).orderBy('created_at','desc').fetch()
+    	let message = yield Message.query().whereRaw("sender_id = ? AND recipient_id = ? OR sender_id = ? AND recipient_id = ?",
+    		[user.id, friend_id, friend_id, user.id]).orderBy('created_at','desc').fetch()
 
     	response.status(200).json(message)
 
